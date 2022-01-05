d41aa047aa3bd4e3a23b6c1e0b410e275c2c8fe8
sajith.rahim <sajith.rahim@outlook.com>
2022-01-05 21:15:30 +0530

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Paths:
+    log: str
+    data: str
+
+
+@dataclass
+class Files:
+    train_data: str
+    train_labels: str
+    test_data: str
+    test_labels: str
+
+
+@dataclass
+class Params:
+    epoch_count: int
+    lr: float
+    batch_size: int
+
