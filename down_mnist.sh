1332c58192b6781cf40c672612cb36cbe759979b
sajith.rahim <sajith.rahim@outlook.com>
2022-02-20 18:12:54 +0530

    Delete down_mnist.sh

--- down_mnist.sh
+++
@@ -1,7 +0,0 @@
-curl --create-dirs -o "data/raw/train-images-idx3-ubyte.gz" http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
-
-curl --create-dirs -o "data/raw/train-labels-idx1-ubyte.gz" http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
-
-curl --create-dirs -o "data/raw/t10k-images-idx3-ubyte.gz" http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
-
-curl --create-dirs -o "data/raw/t10k-labels-idx1-ubyte.gz" http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
